c232e9a17b3d424c712e2cfe4dfdf20dda813e85
I. Gaffling <10500034+gaffling@users.noreply.github.com>
2019-04-01 11:11:25 +0200

    Update function.php

--- function.php
+++ function.php
@@ -18,7 +18,7 @@ function get_var_name(){
 // Say you have this PHP code:
 $FooBar = "a string";
 
-// and need not only the dump but also the Variable Name, you can use this:
+// and need not only the dump of $FooBar, but also the Variable Name, you can use this:
 echo get_var_name($FooBar).' - '.$FooBar;
 
 // which prints: "FooBar - a string"
